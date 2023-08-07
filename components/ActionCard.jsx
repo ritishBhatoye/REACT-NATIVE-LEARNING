bdb9890e0b2fe8710719e460148d4ff50c9e317e
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-08-08 01:44:59 +0530

    NATIVE

--- components/ActionCard.jsx
+++ components/ActionCard.jsx
@@ -1,4 +1,4 @@
-import { View, Text,Linking,Image } from 'react-native'
+import { View, Text,Linking,Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StyleSheet } from 'react-native'
 const ActionCard = () => {
@@ -16,42 +16,89 @@ const ActionCard = () => {
       </View>
       <Image
       source={{
-        uri:'https://images.unsplash.com/photo-1548567117-02328f050eaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
+        uri:'https://images.pexels.com/photos/17788608/pexels-photo-17788608/free-photo-of-couple-standing-against-horse-on-ends-with-riders-and-herd-of-horses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
       }}
       style={styles.cardImage}
       />
       </View>
       <View style={styles.bodyContainer}>
-        <Text>
+        <Text numberOfLines={3}>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Quas minima esse, beatae rerum mollitia excepturi ea 
             tenetur quasi libero deserunt eaque, soluta maxime
             nemo architecto dolor! Neque officia tempore natus?
         </Text>
       </View>
+      <View style={styles.footerContainer}>
+     <TouchableOpacity 
+     onPress={()=>openWebsite('https://reactnative.dev/docs/image')}>
+        <Text style={styles.socialLinks}>Read More</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+     onPress={()=>openWebsite('https://reactnative.dev/docs/image')}
+     >
+        <Text style={styles.socialLinks}>Follow Me</Text>
+      </TouchableOpacity>
+      </View>
     </View>
   )
 }
-
 export default ActionCard
 const styles=StyleSheet.create({
 headingText:{
+    fontSize:24,
+    fontWeight:'bold',
+    paddingHorizontal:8,
 },
 card:{
- 
+    height:360,
+    width:350,
+    borderRadius:6,
+    marginVertical:12,
+    marginHorizontal:16
  },
 elevatedCard:{
-        
+        backgroundColor:'#E07C24',
+        elevation:3,
+        shadowOffset:{
+            width:1,
+            height:1
+        },
+        shadowColor:'#333',
+        shadowOpacity:0.4
 },
 headingContainer:{
-
+height:40,
+flexDirection:'row',
+justifyContent:'center',
+alignItems:'center',
  },
 headerText:{
-
+color:'#000',
+fontSize:16,
+fontWeight:'600'
     },
 cardImage:{
-    height:100,
+    height:190,
 },
-bodyContainer:{},
+bodyContainer:{
+    padding:10,
 },
+footerContainer:{
+padding:8,
+flexDirection:'row',
+alignItems:'center',
+justifyContent:'space-evenly'
+},
+socialLinks:{
+    fontSize:16,
+    color:'#000000',
+    backgroundColor:'#FFF',
+    paddingHorizontal:20,
+    paddingVertical:6,
+    borderRadius:6
+
+}
+},
+
 )
